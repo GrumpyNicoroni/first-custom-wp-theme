781dc9cdc7b01a7ba8f6d2191e1ac7fe93902801
Nicolai Niethe <nicolai.niethe@uksh.de>
2021-10-26 16:46:18 +0200

    Add Bootstrap Nav walker

--- functions.php
+++ functions.php
@@ -91,3 +91,12 @@ function mytheme_register_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'mytheme_register_scripts');
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
--- template-parts/navigation/navigation.php
+++ template-parts/navigation/navigation.php
@@ -1,11 +1,27 @@
+<a class="navbar-brand" href="#">LOGO</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
 <?php
     wp_nav_menu( 
         array( 
             'theme-location'    => 'primary',
-            'menu_class'        => 'menu-wrapper',
-            'container_class'   => 'primary-menu-container',
-            'items_wrap'        => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
-            'fallback_cb'       => false,
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'bs-example-navbar-collapse-1',
+            'menu_class'        => 'navbar-nav mr-auto',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker(),
         )
     );
 ?>
+
+<form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+</form>
+</div>
